1c3bedf8cf2a9579a9d49b1979e98c92a97de034
Matthew Thebert <matt@thebert.com>
2014-01-11 13:47:43 -0500

    eventData

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,7 +4,11 @@ eventsApp.controller('EventController',
 	// eventData is a service we created to hold our data - it is being passed in, like scope, so we have access to it
 	function EventController($scope, eventData){
 		$scope.sortorder= '-upVoteCount';
-		$scope.event= eventData.event;
+		// so we have our eventData model, calling the getEvent function and the callback is the function we pass the event to
+		// and the passback gets assigned to the $scope.event
+		eventData.GetEvent(function(event){
+			$scope.event=event;
+		});
 		$scope.upVoteSession = function(session){
 			session.upVoteCount++;
 		}
