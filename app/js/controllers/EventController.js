afee6fa339284bd08ba5821535fdd33e18389f75
Matthew Thebert <matt@thebert.com>
2014-01-10 22:37:21 -0500

    EventController session info

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,7 +11,31 @@ eventsApp.controller('EventController',
 				city: 'Mountain View',
 				province: 'CA'
 			},
-			imageUrl: 'img/angularjs-logo.png'
+			imageUrl: 'img/angularjs-logo.png',
+			sessions: [
+				{
+					name: 'Directives Masterclass',
+					creatorName: 'Bob Smith',
+					duration: '1 hr',
+					level: 'Advanced',
+					abstract: 'In this session we will learn the ins and outs of directives!'
+				},
+				{
+					name: 'Scopes for fun and profit',
+					creatorName: 'John Doe',
+					duration: '30 min',
+					level: 'Introductory',
+					abstract: 'This session will take a closer look at scopes.  Learn what they do and how they can work for you'
+
+				},
+				{
+					name: 'Well Behaved Controllers',
+					creatorName: 'Jane Doe',
+					duration: '2 hours',
+					level: 'Intermediate',
+					abstract: 'Controllers are the beginning of everything Angular does.  Learn how to craft controllers that will win the respect of your friends and neighbors.'
+				}
+			]
 
 		}
 	}
