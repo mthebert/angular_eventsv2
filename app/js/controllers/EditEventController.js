a6f9447f893b933b4355be68f78551533a58d958
Matthew Thebert <matt@thebert.com>
2014-01-11 01:56:04 -0500

    edit event validation

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,8 +2,12 @@
 
 eventsApp.controller('EditEventController', 
 	function EditEventController($scope){
-		$scope.saveEvent = function(event){
-			window.alert('event: ' + event.name + ' saved');
+		// newEventForm is from form name="newEventForm"
+		$scope.saveEvent = function(event, newEventForm){
+			console.log(newEventForm);
+			if(newEventForm.$valid){
+				window.alert('event: ' + event.name + ' saved');
+			}
 		};
 		$scope.cancelEdit = function(){
 			window.location = "/EventDetails.html";
