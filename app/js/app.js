0d0a43542e882d1df63dd70d021d35b845e4c882
Matthew Thebert <matt@thebert.com>
2014-01-18 13:58:44 -0500

    more routes

--- app/js/app.js
+++ app/js/app.js
@@ -14,6 +14,7 @@ var eventsApp = angular.module('eventsApp', ['ngResource'])
 		});
 		$routeProvider.when('/event/:eventId',
 		{
+			foo: 'bar',
 			templateUrl: 'templates/EventDetails.html',
 			controller: 'EventController'
 		});	
--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,13 +2,21 @@
 
 eventsApp.controller('EventController', 
 	// eventData is a service we created to hold our data - it is being passed in, like scope, so we have access to it
-	function EventController($scope, eventData, $routeParams){
+	function EventController($scope, eventData, $routeParams, $route){
 		$scope.sortorder= '-upVoteCount';
 		// so we have our eventData model, calling the getEvent function and the callback is the function we pass the event to
 		// and the passback gets assigned to the $scope.event
 
 		// this gives you access to the event object, the $scope.event is just a promise
 		$scope.event=eventData.getEvent($routeParams.eventId);
+		// this is set in the $route
+		console.log($route.current.foo);
+		// this is set by querystring
+		console.log($route.current.params.barval);
+		//this is set in the rest id
+		console.log($route.current.params.eventId);
+		// the difference with pathParams only has the route, not the querystring
+		console.log($route.current.pathParams.eventId);
 		$scope.event.then(
 			function(event) {console.log(event);},
 			function(response) {console.log(response)
@@ -25,5 +33,8 @@ eventsApp.controller('EventController',
 		$scope.scrollToSession = function(){
 			$anchorScroll();
 		}
+		$scope.reload = function(){
+			$route.reload();
+		}		
 	}
 );
